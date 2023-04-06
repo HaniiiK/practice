c916c662acacb2a4248172b1985e3dde5ab6d8d6
HaeunKim <rlagkdms26@gmail.com>
2023-04-06 12:31:14 +0900

    Test1_Commit1

---
+++ Practice02/src/com/inherit/test01/Run.java
@@ -0,0 +1,14 @@
+package com.inherit.test01;
+
+public class Run {
+	public static void main(String[] args) {
+		Student st = new Student();
+		
+		st.major = "softward";
+		st.num = 11148;
+		System.out.println(st.major);
+		System.out.println(st.num);
+
+	}
+
+}
---
+++ Practice02/src/com/inherit/test01/Student.java
@@ -0,0 +1,8 @@
+package com.inherit.test01;
+
+public class Student {
+	public String major;
+	public int num;
+	
+	
+}
