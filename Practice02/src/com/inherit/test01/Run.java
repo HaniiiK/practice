f32dce6c152c616c8467e187fbdc73142318352c
HaeunKim <rlagkdms26@gmail.com>
2023-04-06 12:33:45 +0900

    Test02_Commit1

--- Practice02/src/com/inherit/test01/Run.java
+++ Practice02/src/com/inherit/test01/Run.java
@@ -4,10 +4,10 @@ public class Run {
 	public static void main(String[] args) {
 		Student st = new Student();
 		
-		st.major = "softward";
-		st.num = 11148;
-		System.out.println(st.major);
-		System.out.println(st.num);
+		st.setMajor("softward");
+		st.setNum(11148);
+		System.out.println(st.getMajor());
+		System.out.println(st.getNum());
 
 	}
 
--- Practice02/src/com/inherit/test01/Student.java
+++ Practice02/src/com/inherit/test01/Student.java
@@ -1,8 +1,26 @@
 package com.inherit.test01;
 
 public class Student {
-	public String major;
-	public int num;
+	private String major;
+	private int num;
 	
+	public Student() {}
+	public Student(String major, int num) {
+		super();
+		this.major = major;
+		this.num = num;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
 	
 }
