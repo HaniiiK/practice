19adbebbaa58246f7cf413b62b3d0dad8cb7f875
HaeunKim <rlagkdms26@gmail.com>
2023-04-13 17:58:37 +0900

    Commit1

---
+++ Practice02/src/com/day07/silsub/test01/MTest.java
@@ -0,0 +1,25 @@
+package com.day07.silsub.test01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class MTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Vector<Integer> v = new Vector<>();
+		
+		int n = 0;
+		System.out.print("정수(-1이 입력될 때까지)>> ");
+		while(n!=-1) {
+			n = sc.nextInt();
+			v.add(n);
+		}
+		Object[] arr = v.toArray();
+		Arrays.sort(arr);
+		
+		System.out.println(arr[arr.length-1]);
+	}
+}
